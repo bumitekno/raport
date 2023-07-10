20ab06e04142f78e4280f6d9bf8357661db4a9e6
mucharom tzaka <mucharom92@yahoo.com>
2023-07-10 12:16:30 +0700

    console api buku induk

--- app/Console/Commands/SyncData.php
+++ app/Console/Commands/SyncData.php
@@ -3,6 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use DateTime;
+use App\Models\Level;
+
 
 class SyncData extends Command
 {
@@ -28,6 +33,10 @@ class SyncData extends Command
     public function handle()
     {
 
+        $output = new ConsoleOutput();
+        $datetime = new DateTime();
+        $timestamp = $datetime->format('Y-m-d H:i:s');
+
         //handle sync api buku induk 
         if (!empty(env('API_BUKU_INDUK'))) {
 
@@ -36,7 +45,12 @@ class SyncData extends Command
              */
 
             $url_api_levels = env('API_BUKU_INDUK') . '/api/master/levels';
+            $response_api_level = Http::get($url_api_levels);
+            $resposnse_collection_levels = $response_api_level->collect();
+            $output->writeln('info:' . $resposnse_collection_levels);
 
+        } else {
+            $output->writeln('error: API URL BUKU INDUK tidak di ketahui ');
         }
 
         return Command::SUCCESS;
